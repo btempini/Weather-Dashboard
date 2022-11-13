2817bf7d7b457d2ef1211582b9963186396500c1
Bryan Tempini <mistatempini@gmail.com>
2022-11-13 11:33:47 -0500

    testing click event

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var input = document.querySelector(".input")
+var inputBtn = document.querySelector(".search")
+
+inputBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  console.log("Hello")
+})
